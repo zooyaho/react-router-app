bd26c51d52dcb6689af18707e9c186952872a3fd
zooyaho <jiwoo1268@naver.com>
2022-12-22 20:23:41 +0900

    feat: Add sorted citation list feature

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,13 +1,44 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 
-import QuoteItem from './QuoteItem';
-import classes from './QuoteList.module.css';
+import QuoteItem from "./QuoteItem";
+import classes from "./QuoteList.module.css";
+
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      // 오름차순
+      console.log("오름차순");
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
 
 const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search); // JS의 기본 생성자 함수, key와 value를 갖는 객체를 생성한다.
+  const isSortingAscending = queryParams.get("sort") === "asc"; // key를 사용하여 해당 값을 가져옴.
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
+  const changeSortingHandler = () => {
+    // history객체는 URL을 바꿀 수 있게 하며,
+    // location객체는 최근 로드된 페이지와 URL에 대한 정보가 있음. -> search속성에서 쿼리매개변수를 확인할 수 있음
+    history.push(`/quotes?sort=${isSortingAscending ? "desc" : "asc"}`); // 페이지 컴포넌트는 재평가가 됨
+  };
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>
+          Sort {isSortingAscending ? "Descending" : "Ascending"}
+        </button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
