244ec41ecd39b74ee1a9136d2b028ddc47ddb86f
zooyaho <jiwoo1268@naver.com>
2022-12-26 16:54:18 +0900

    feat: Add comment and print comment

--- src/components/comments/NewCommentForm.js
+++ src/components/comments/NewCommentForm.js
@@ -1,26 +1,45 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from "react";
+import useHttp from "../../hooks/use-http";
+import { addComment } from "../../lib/api";
 
-import classes from './NewCommentForm.module.css';
+import classes from "./NewCommentForm.module.css";
+import LoadingSpinner from "../UI/LoadingSpinner";
 
-const NewCommentForm = (props) => {
+const NewCommentForm = ({ onAddedComment, quoteId }) => {
   const commentTextRef = useRef();
+  const { sendRequest, status, error } = useHttp(addComment);
+
+  useEffect(() => {
+    if (status === "completed" && !error) {
+      // comment 추가 완료 시 comment list 출력
+      onAddedComment();
+    }
+  }, [error, onAddedComment, status]);
 
   const submitFormHandler = (event) => {
     event.preventDefault();
 
     // optional: Could validate here
 
-    // send comment to server
+    sendRequest({
+      quoteId,
+      commentData: { text: commentTextRef.current.value },
+    });
   };
 
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
+      {status === "pending" && (
+        <div className="centered">
+          <LoadingSpinner />
+        </div>
+      )}
       <div className={classes.control} onSubmit={submitFormHandler}>
-        <label htmlFor='comment'>Your Comment</label>
-        <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
+        <label htmlFor="comment">Your Comment</label>
+        <textarea id="comment" rows="5" ref={commentTextRef}></textarea>
       </div>
       <div className={classes.actions}>
-        <button className='btn'>Add Comment</button>
+        <button className="btn">Add Comment</button>
       </div>
     </form>
   );
